718dae7596eddf253026bc92b3f63aeb2a85dcb5
Kevin Henderson <pyspud@gmail.com>
2011-04-19 16:09:06 +0100

    Update location on change in GPS
    Use Criteria to select location. Update text when location changes.
    
    from chapter8 page253

--- src/uk/co/kalgan/wherethehell/WhereTheHell.java
+++ src/uk/co/kalgan/wherethehell/WhereTheHell.java
@@ -2,7 +2,9 @@ package uk.co.kalgan.wherethehell;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -17,11 +19,30 @@ public class WhereTheHell extends Activity {
         String context = Context.LOCATION_SERVICE;
         locationManager = (LocationManager)getSystemService(context);
         
-        String provider = LocationManager.GPS_PROVIDER;
-        Location location = locationManager.getLastKnownLocation(provider);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        String provider = locationManager.getBestProvider(criteria, true);
         
+        Location location = locationManager.getLastKnownLocation(provider);
         updateWithNewLocation(location);
+        
+        locationManager.requestLocationUpdates(provider, 2000, 10, locationListener);
     }
+    
+    private final LocationListener locationListener = new LocationListener() {
+    	public void onLocationChanged(Location _location) {
+    		updateWithNewLocation(_location);
+    	}
+    	public void onProviderDisabled(String _provider) {
+    		updateWithNewLocation(null);
+    	}
+    	public void onProviderEnabled(String _provider) { }
+    	public void onStatusChanged(String _provider, int _status, Bundle _extras) { }
+	};
     
     private void updateWithNewLocation(Location _location) {
     	String latLonString;
