49541f967d43446f481ed545a56a0d2ce12adee2
Kevin Henderson <pyspud@gmail.com>
2011-04-19 13:59:49 +0100

    Initial files for WhereTheHell App.
    Files generated by Eclipse for an example WhereAmI? app from
    Professional Android 2 Application Development by Reto Meier

---
+++ README.md
@@ -0,0 +1,10 @@
+WhereTheHell
+============
+
+This project is for learning Android development.
+
+>SHOULD NOT BE USED AS WILL KILL CYGNETS.
+
+Application based on the Example WhereAmI? application from Professional Android 2 Application Development Book by Reto Meier
+
+
---
+++ src/uk/co/kalgan/wherethehell/WhereTheHell.java
@@ -0,0 +1,13 @@
+package uk.co.kalgan.wherethehell;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WhereTheHell extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
