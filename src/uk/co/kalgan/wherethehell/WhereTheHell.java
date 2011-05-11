bcdc24526911bff95975e50dd7877f77ecbb0959
Kevin Henderson <pyspud@gmail.com>
2011-05-11 16:12:03 +0100

    Lookup the current address
    Use the Geocoder to get the current address at the coordinates and
    displaythat address.
    
    from chapter8 page259

--- src/uk/co/kalgan/wherethehell/WhereTheHell.java
+++ src/uk/co/kalgan/wherethehell/WhereTheHell.java
@@ -1,8 +1,14 @@
 package uk.co.kalgan.wherethehell;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Context;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -50,13 +56,33 @@ public class WhereTheHell extends Activity {
     	
     	myLocationText = (TextView)findViewById(R.id.myLocationText);
     	
+    	String addressString = "No address found";
+    	
     	if (_location != null) {
     		double lat = _location.getLatitude();
     		double lon = _location.getLongitude();
     		latLonString = "Lat: " + lat + "\nLon: " + lon;
+    		
+    		Geocoder gc = new Geocoder(this, Locale.getDefault());
+    		try {
+    			List<Address> addresses = gc.getFromLocation(lat, lon, 1);
+    			StringBuilder sb = new StringBuilder();
+    			
+    			if (addresses.size() > 0) {
+    				Address address = addresses.get(0);
+    				
+    				for ( int i = 0; i < address.getMaxAddressLineIndex(); i++)
+    					sb.append(address.getAddressLine(i)).append("\n");
+    				sb.append(address.getLocality()).append("\n");
+    				sb.append(address.getPostalCode()).append("\n");
+    				sb.append(address.getCountryName()).append("\n");
+    			}
+    			addressString = sb.toString();
+    		} catch (IOException e) {}
     	} else {
     		latLonString = "No location found.";
     	}
-    	myLocationText.setText("Your location is:\n"+ latLonString);
+    	myLocationText.setText("Your location is:\n"+ latLonString + "\n"
+    			+ addressString);
     }
 }
